fa3957169d63610acf4df65b4c747ec432d26c37
Danial Mahmoudi <danialmahmoudi@danials-air.attlocal.net>
2023-07-08 19:32:14 -0400

    heroku commit

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -48,7 +48,7 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!');
         },
         // remove a book
-        removeBook: async (parent, { bookId} , context) => {
+        removeBook: async (parent, { bookId } , context) => {
             if (context.user) {
                 const updateBook = await User.findOneAndUpdate(
                     {_id: context.user._id},
